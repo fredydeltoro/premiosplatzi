089bea9406f50c4fc769338d982f4011e64e6c89
Freddy Monroy <fredydeltoro@gmail.com>
2022-06-24 13:31:44 -0500

    More tests

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -13,3 +13,14 @@ class QuestionModelTests(TestCase):
     time = timezone.now() + datetime.timedelta(days=30)
     future_question = Question(question_text="¿Quien es el mejore cd de platzi?", pub_date=time)
     self.assertIs(future_question.was_publish_recently(), False)
+  
+  def test_was_published_recently_with_present_questions(self):
+    """was_published_recently returns True for questions whose pub_date is in the present"""
+    present_question = Question(question_text="¿Quien es el mejore cd de platzi?", pub_date=timezone.now())
+    self.assertIs(present_question.was_publish_recently(), True)
+  
+  def test_was_published_recently_with_past_questions(self):
+    """was_published_recently returns False for questions whose pub_date is in the past"""
+    time = timezone.now() - datetime.timedelta(days=30)
+    past_question = Question(question_text="¿Quien es el mejore cd de platzi?", pub_date=time)
+    self.assertIs(past_question.was_publish_recently(), False)
